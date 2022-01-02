66b0c39ba51100ce35f4d0e768074109dcd50853
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2022-01-02 20:53:41 -0300

    Aulas 47 e 48 - Criando URL amigáveis

--- Rotas/App_Start/RouteConfig.cs
+++ Rotas/App_Start/RouteConfig.cs
@@ -14,6 +14,25 @@ namespace Rotas
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Todas as Notícias",
+                url: "noticias/",
+                defaults: new { Controller = "Home", Action = "TodasAsNoticias" }
+                );
+
+            routes.MapRoute(
+               name: "Categoria Específica",
+               url: "noticias/{categoria}",
+               defaults: new { Controller = "Home", Action = "MostrarCategoria" }
+               );
+
+            routes.MapRoute(
+              name: "Mostrar Notícias",
+              url: "noticias/{categoria}/{titulo}-{noticiaId}",
+              defaults: new { Controller = "Home", Action = "MostrarNoticias" }
+              );
+
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
